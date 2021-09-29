834bdeca96a536c353ff69e6db08fc69c366ca02
danielle076 <d.vandenakker@novi-education.nl>
2021-09-29 12:55:27 +0200

    first commit

---
+++ src/test/java/nl/danielle/file_demo/FileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.danielle.file_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
